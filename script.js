22fb912ac2e0fb3387151e9a27bdb05270acced5
ar_parvez <arparvez50@gmail.com>
2023-12-19 20:37:17 +0600

    Don't know, what happened

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ import Address from "./Address.js";
 
 let likhonsAddress = new Address("Mirpur", "Dhaka", "Bangladesh");
 let likhon = new Human("Abdullah Likhon", 28, "Male", likhonsAddress);
-
+let output = document.querySelector('.object__output')
 
 console.log(obj, likhon);
+
+output.innerText = JSON.stringify(likhon);
+console.log("Hello worl")
